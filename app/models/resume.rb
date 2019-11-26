5d71cd01c4818716ab8d16eed157c9f5e57a5ace
Debora Alves <foonatic@gmail.com>
2019-11-26 13:20:03 -0300

    Resume validation done

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,3 +1,10 @@
 class Resume < ApplicationRecord
   belongs_to :candidate
+
+  validates :candidate_id, presence: true
+  validates :resume_language, inclusion: { in: ["english","spanish","portuguese", "french", "german", "japanese", "chinese"], allow_nil: false }
+  validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :education, presence: true
+  validates :skills, length: { minimum: 2 }, presence: true
+  validates :experience, presence: true
 end
