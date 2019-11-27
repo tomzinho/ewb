9c1ab46bbba9b5ef3398058db7a40c1db308cd61
Debora Alves <foonatic@gmail.com>
2019-11-27 15:19:04 -0300

    Job ok

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,11 @@
 class Job < ApplicationRecord
   belongs_to :company
+
+  validates :company_id, presence: true
+  validates :title, presence: true, length: { minimum: 10 }
+  validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :location, presence: true
+  # validates :latitude,
+  # validates :longitude,
+
 end
