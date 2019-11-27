0f90b92411028471b206eee7ddf4d7f3047c3140
Joao Alves <joaol.alves@gmail.com>
2019-11-27 13:30:17 -0300

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
-require 'faker'
-
 puts "Removing applies..."
   Apply.destroy_all
 puts "Removing resumes database..."
   Resume.destroy_all
 puts "Removing candidates database..."
   Candidate.destroy_all
-​puts "Removing jobs database..."
+puts "Removing jobs database..."
   Job.destroy_all
 puts "Removing companies database..."
   Company.destroy_all
@@ -49,41 +47,46 @@ w_auths = %w(br us can eu)
 puts "Describing the candidates"
 
 candidates.each do |candidate|
-  candidate.small_desc:   Faker::Lorem.paragraph(sentence_count: 20)
-  candidate.github_link:  "github.com/#{Faker::Internet.username}"
-  candidate.address:      Faker::Address.street_address
-  candidate.work_auth:    w_auths.sample
-  candidate.save!
+  Candidate.create!(
+  user_id:      candidate.id,
+  small_desc:   Faker::Lorem.paragraph(sentence_count: 10),
+  github_link:  "github.com/#{Faker::Internet.username}",
+  address:      Faker::Address.street_address,
+  work_auth:    w_auths.sample
+  )
 end
 
 
 # campanies -----------------------------------------------------------
 puts "Describing the Companies"
 
-companies.eachs do |company|
+companies.each do |company|
   name = Faker::Company.name
-  company.website:       Faker::Internet.url,
-  company.facebook_link: "www.facebook.com/#{name}"
-  company.twitter_link:  "https://twitter.com//#{name}"
-  company.cnpj:          Faker::Number.number(digits: 14)
-  company.address:       Faker::Address.street_address
-  # company.logo:        # cloudinary?
-  # company.banner:      # cloudinary?
-  company.term_of_use:   true
-  company.source:        ["friends", "Google", "Linkedin", "Facebook"].sample
-  company.save!
+  Company.create!(
+    user_id:       company.id,
+    website:       Faker::Internet.url,
+    facebook_link: "www.facebook.com/#{name}",
+    twitter_link:  "https://twitter.com//#{name}",
+    # logo:        # cloudinary?,
+    # banner:      # cloudinary?,
+    cnpj:          Faker::Number.number(digits: 6),
+    address:       Faker::Address.street_address,
+    term_of_use:   true,
+    source:        ["friends", "Google", "Linkedin", "Facebook"].sample
+  )
 end
 
 # jobs -----------------------------------------------------------
 puts "Creating new Jobs"
 
 10.times do
-  company_job =      companies.sample
+  all_companies = Company.all
+  company_job = all_companies.sample
   Job.create!(
-  company_id:    company_job.id
-  title:         Faker::Lorem.sentence(word_count: 3)
-  description:   Faker::Lorem.sentence(word_count: 20)
-  location:      company_job.address
+  company_id:    company_job.id,
+  title:         Faker::Lorem.sentence(word_count: 3),
+  description:   Faker::Lorem.sentence(word_count: 20),
+  location:      company_job.address,
   status:        true
   )
 end
@@ -92,18 +95,18 @@ end
 # resumes -----------------------------------------------------------
 puts "Creating new Resumes"
 
-candidate.each do |candidate|
+all_candidates = Candidate.all
+
+all_candidates.each do |candidate|
   Resume.create!(
-    candidate_id:    candidate.id
-    resume_language: "english"
-    description:     Faker::Lorem.sentence(word_count: 20)
-    education:       Faker::University.name
-    skills:          "Communication, Teamwork, Problem solving, Initiative and enterprise, Planning and organising, Self-management, Learning"
-    experience:      Faker::Lorem.sentence(word_count: 10)
+    candidate_id:    candidate.id,
+    resume_language: "english",
+    description:     Faker::Lorem.sentence(word_count: 10),
+    education:       Faker::University.name,
+    skills:          "Communication, Teamwork, Problem solving, Initiative and enterprise, Planning and organising, Self-management, Learning",
+    experience:      Faker::Lorem.sentence(word_count: 10),
     # video:         # cloudinary?
   )
 end
 
 puts 'Finished!'
-
-
