14c3068e7a134dea8e0bab9a46d10e4c7bb4d799
Joao Alves <joaol.alves@gmail.com>
2019-11-28 11:13:38 -0300

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :candidates, only: %i[edit update]
   resources :companies, only: %i[edit update]
-  resources :jobs, only: [ :new, :create ]
+  resources :jobs, only: %i[new create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
