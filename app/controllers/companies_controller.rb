ed32f256863509874579e6fc4324bc43e82ebc8a
Joao Alves <joaol.alves@gmail.com>
2019-11-28 11:53:23 -0300

    edit and update working

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,7 +4,14 @@ class CompaniesController < ApplicationController
   end
 
   def update
-    raise
+    @company = Company.find(params[:id])
+    @company = Company.update(company_params)
+    redirect_to edit_company_path(@company)
   end
 
+  private
+
+  def company_params
+    params.require(:company).permit(:website, :facebook_link, :twitter_link, :logo, :banner, :cnpj, :address, :source)
+  end
 end
