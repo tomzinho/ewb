9bea025a61e652811dabb184d91ce7da66d95c72
Joao Alves <joaol.alves@gmail.com>
2019-11-28 17:59:38 -0300

    Added nem column name to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_201907) do
+ActiveRecord::Schema.define(version: 2019_11_28_205302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_201907) do
     t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
