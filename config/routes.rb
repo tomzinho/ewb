e81aa600996e6e665a86876764a16f0df430aefd
Joao Alves <joaol.alves@gmail.com>
2019-12-03 00:38:54 -0300

    created callback for LikedIn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+      controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
   get '/companies/:id/dashboard', to: 'companies#dashboard', as: 'dashboard'
