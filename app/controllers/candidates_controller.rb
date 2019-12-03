e72a58d1876757c09dd8d7670bd780c7546cfc51
Tom McCutchen <tom.mccutchen@gmail.com>
2019-12-03 17:11:37 -0200

    improvements to candidates, still a mess

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,17 +1,21 @@
 class CandidatesController < ApplicationController
+  def edit
+    @candidate = Candidate.find(params[:id])
+  end
 
-def edit
-  @candidate = Candidate.find(params[:id])
-end
+  def update
+    @candidate = Candidate.find(params[:id])
+    @candidate.update(candidate_params)
+    redirect_to edit_candidate_path(@candidate)
+  end
 
-def update
-  @candidate = Candidate.find(params[:id])
-  @candidate.update(candidate_params)
-  redirect_to edit_candidate_path(@candidate)
-end
+  def dashboard
+    @candidate = Candidate.find(params[:id])
+  end
 
-private
-def candidate_params
-  params.require(:candidate).permit(:photo, :photo_cache, :small_desc, :address, :github_link, :workauth_bra, :workauth_usa, :workauth_eu, :workauth_can, :workauth_aus, :workauth_gb)
-end
+  private
+
+  def candidate_params
+    params.require(:candidate).permit(:photo, :photo_cache, :small_desc, :address, :github_link, :workauth_bra, :workauth_usa, :workauth_eu, :workauth_can, :workauth_aus, :workauth_gb)
+  end
 end
