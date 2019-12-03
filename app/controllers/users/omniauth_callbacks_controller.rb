0df675a0a1bde74d52bd3bfeb7345f415cc949ca
Joao Alves <joaol.alves@gmail.com>
2019-12-03 00:38:54 -0300

    created callback for LikedIn

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def linkedin
+    user = User.find_for_linkedin_oauth(request.env['omniauth.auth'])
+
+        if user.persisted?
+          sign_in_and_redirect user, event: :authentication
+          set_flash_message(:notice, :success, kind: 'Linkedin') if is_navigational_format?
+        else
+          session['devise.linkedin_data'] = request.env['omniauth.auth']
+          redirect_to new_user_registration_url
+        end
+      end
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
