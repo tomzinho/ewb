ec9bcb3fe7e8a544de2563899c5f76afdd87d717
Debora Alves <foonatic@gmail.com>
2019-12-03 21:27:08 -0300

    home and jobs dashboard done

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :candidates, only: %i[edit update]
   resources :companies, only: %i[show edit update dashboard]
   resources :resumes
-  resources :jobs, only: %i[show new create]
+  resources :jobs, only: %i[show new create index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
