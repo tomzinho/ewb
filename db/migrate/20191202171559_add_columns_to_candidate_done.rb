8162df3fa7e294d48394a5d080494488789e32eb
Joao Alves <joaol.alves@gmail.com>
2019-12-05 16:17:38 -0300

    fixed migrations

--- db/migrate/20191202171559_add_columns_to_candidate_done.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToCandidate < ActiveRecord::Migration[5.2]
-  def change
-    add_column :candidates, :first_name, :string
-    add_column :candidates, :last_name, :string
-  end
-end
