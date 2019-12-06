c2990422c9c08c01a2602e8c59629dac1802a233
Debora Alves <foonatic@gmail.com>
2019-12-06 00:14:41 -0300

    company not ok

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,6 +3,10 @@
     @company = Company.find(params[:id])
   end
 
+  def show
+    @company = Company.find(params[:id])
+  end
+
   def update
     @company = Company.find(params[:id])
     @company.update(company_params)
