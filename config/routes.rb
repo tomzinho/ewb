fab99b15dc7ad33eaf7f67fe89418653ef8113b9
Joao Alves <joaol.alves@gmail.com>
2019-12-07 00:13:15 -0300

    list of pitchs working

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   patch '/role/:id', to: 'pages#role_update'
   get '/candidates/:id/dashboard', to: 'candidates#dashboard', as: 'candidate_dashboard'
   get '/companies/:id/dashboard', to: 'companies#dashboard', as: 'company_dashboard'
+  get '/jobs/:id/list', to: 'applies#list', as: 'list'
 
   resources :candidates, only: %i[edit update dashboard index]
   resources :companies, only: %i[show edit update dashboard show]
